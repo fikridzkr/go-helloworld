cfe4847fea6d222836232103dc54335423485423
Fikri Dzakir <fikridzkr@gmail.com>
2022-05-05 18:27:52 +0700

    Type declaration

--- hello.go
+++ hello.go
@@ -6,14 +6,12 @@ func main() {
 	const TITLE = "PROFILE"
 	var a, b, c, d int = 1, 3, 5, 7
 	var age int32
-	firstName := "Fikri"
 
 	age = 18
 
 	fmt.Println(a, b, c, d)
 	fmt.Println(TITLE)
 	fmt.Println(age)
-	fmt.Println((string(firstName[0])), "F")
 	fmt.Println("Hello, World!")
 
 	array1 := [...]int{4, 5, 6, 7, 8}
@@ -22,4 +20,13 @@ func main() {
 	fmt.Println(array2)
 	fmt.Println(cap(array1))
 
+	// Type Declaration
+	type isMarried bool
+	type name string
+
+	var married isMarried = true
+	var firstName name = "Fikri"
+
+	fmt.Println(married)
+	fmt.Println(firstName)
 }
