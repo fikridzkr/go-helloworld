b0685b80787c9485156daf588f60f60c8eed6e61
Fikri Dzakir <fikridzkr@gmail.com>
2022-05-06 20:09:54 +0700

    math operation

--- hello.go
+++ hello.go
@@ -3,30 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	const TITLE = "PROFILE"
-	var a, b, c, d int = 1, 3, 5, 7
-	var age int32
+	nilaiA := 20
+	nilaiB := 10
+	nilaiC := nilaiA + nilaiB
+	nilaiD := nilaiA - nilaiB
+	nilaiE := nilaiA / nilaiB
+	nilaiF := nilaiA * nilaiB
+	nilaiG := nilaiA % nilaiB
 
-	age = 18
-
-	fmt.Println(a, b, c, d)
-	fmt.Println(TITLE)
-	fmt.Println(age)
-	fmt.Println("Hello, World!")
-
-	array1 := [...]int{4, 5, 6, 7, 8}
-	array2 := [2]int{2, 3}
-
-	fmt.Println(array2)
-	fmt.Println(cap(array1))
-
-	// Type Declaration
-	type isMarried bool
-	type name string
-
-	var married isMarried = true
-	var firstName name = "Fikri"
-
-	fmt.Println(married)
-	fmt.Println(firstName)
+	nilaiB++
+	fmt.Println(nilaiB)
+	fmt.Println("===================")
+	fmt.Println(nilaiC)
+	fmt.Println(nilaiD)
+	fmt.Println(nilaiE)
+	fmt.Println(nilaiF)
+	fmt.Println(nilaiG)
 }
