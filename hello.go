51510189c3de4ae4404fe890780285045c25650e
Fikri Dzakir <fikridzkr@gmail.com>
2022-05-08 09:11:08 +0700

    comparison operation

--- hello.go
+++ hello.go
@@ -5,18 +5,9 @@ import "fmt"
 func main() {
 	nilaiA := 20
 	nilaiB := 10
-	nilaiC := nilaiA + nilaiB
-	nilaiD := nilaiA - nilaiB
-	nilaiE := nilaiA / nilaiB
-	nilaiF := nilaiA * nilaiB
-	nilaiG := nilaiA % nilaiB
+	resultEqual := nilaiA == nilaiB
+	resultNotEqual := nilaiA != nilaiB
 
-	nilaiB++
-	fmt.Println(nilaiB)
-	fmt.Println("===================")
-	fmt.Println(nilaiC)
-	fmt.Println(nilaiD)
-	fmt.Println(nilaiE)
-	fmt.Println(nilaiF)
-	fmt.Println(nilaiG)
+	fmt.Println(resultEqual)
+	fmt.Println(resultNotEqual)
 }
